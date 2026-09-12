b9db489e1be9e58e848359b4a4597f13f670f58f
adnx-xn <adnankhanaus@gmail.com>
2026-09-12 20:01:09 +0530

    Revert "added pages and updated navbar buton"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,6 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
 import Visualization from './pages/Visualization';
 import About from './pages/About';
-import Temperature from './pages/Temperature';
-import Salinity from './pages/Salinity';
-import VO from './pages/VO';
-import UO from './pages/UO';
 import './App.css';
 
 function App() {
@@ -15,22 +11,9 @@ function App() {
   const renderPage = () => {
     switch (currentPage) {
       case 'home':
-        return <Home onNavigate={setCurrentPage} />;
+        return <Home />;
       case 'about':
-      case '/about':
-        return <About onNavigate={setCurrentPage} />;
-      case 'learn-more/temperature':
-      case '/learn-more/temperature':
-        return <Temperature onNavigate={setCurrentPage} />;
-      case 'learn-more/salinity':
-      case '/learn-more/salinity':
-        return <Salinity onNavigate={setCurrentPage} />;
-      case 'learn-more/vo':
-      case '/learn-more/vo':
-        return <VO onNavigate={setCurrentPage} />;
-      case 'learn-more/uo':
-      case '/learn-more/uo':
-        return <UO onNavigate={setCurrentPage} />;
+        return <About />;
       case 'visualization':
       default:
         return <Visualization />;
